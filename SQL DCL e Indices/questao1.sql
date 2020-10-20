a704b7c2983a51a086e3e98c710a84e4ffb6a053
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-20 19:49:41 -0300

    Add questao 1

---
+++ SQL DCL e Indices/questao1.sql
@@ -0,0 +1,3 @@
+-- 1 - Faça o comando de Criação do Banco de Dados acima informando a codificação de caracteres e o collate.
+
+CREATE DATABASE `EmpresaDB` CHARACTER SET utf8 COLLATE utf8_general_ci;
