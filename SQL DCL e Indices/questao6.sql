88a653e19821d077d793b3f7301610f2bde33a5d
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-20 20:35:38 -0300

    Update questao 6

--- SQL DCL e Indices/questao6.sql
+++ SQL DCL e Indices/questao6.sql
@@ -7,3 +7,5 @@ CREATE USER 'empresa_supevisor'@'localhost' IDENTIFIED BY ‘186372526';
 
 CREATE VIEW FuncSemSalario AS SELECT codigo, nome, sexo, datanasc, supervisor, depto FROM Funcionario;
 
+GRANT SELECT ON EmpresaDB.FuncSemSalario Atividade, Atividade_membro, Projeto TO empresa_supervisor;
+
