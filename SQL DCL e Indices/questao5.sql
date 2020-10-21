ec6bc67b5694e42c576fcefd497fde3bb4393982
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-20 21:37:57 -0300

    Respondendo questao 5

--- SQL DCL e Indices/questao5.sql
+++ SQL DCL e Indices/questao5.sql
@@ -1,3 +1,7 @@
 -- 5 - Crie um usuário chamado empresa_gerente para o banco de dados EmpresaDB. 
 --     Este usuário deve ter privilégios apenas de fazer consultas em todas as tabelas. 
 --     Ele pode inserir e atualizar as tabelas Equipe, Membro, Atividade e AtividadeProjeto.
+
+CREATE USER 'empresa_gerente'@'localhost' IDENTIFIED BY 'kjhftdrrssi568';
+GRANT SELECT ON ALL TABLES IN ‘EmpresaDB’ TO 'empresa_gerente'@'localhost';
+GRANT INSERT, UPDATE (Equipe, Membro, Atividade, AtividadeProjeto) TO empresa_gerente;
