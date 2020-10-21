5acab993cff5b5d2193be2353ceca7f6085fe558
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-20 22:12:10 -0300

    Update questao 8

--- SQL DCL e Indices/questao8.sql
+++ SQL DCL e Indices/questao8.sql
@@ -1,7 +1,7 @@
 -- 8 - Explique o funcionamento de um índice e como ele é implementado no SGBD.
 
-Índice é um recurso utilizado pelos BDs para a rápida localização de registros em tabelas.
-Quando cria um  arquivo  de  índice  para a coluna de uma tabela, 
-a consulta deixa de ser sequencial e o registro é localizado mais rapidamente. 
-Os arquivos de índices são menores que as tabelas e, por essa razão, ocupam relativamente pouco espaço na memória principal.
+-- Índice é um recurso utilizado pelos BDs para a rápida localização de registros em tabelas.
+-- Quando cria um  arquivo  de  índice  para a coluna de uma tabela, 
+-- a consulta deixa de ser sequencial e o registro é localizado mais rapidamente. 
+-- Os arquivos de índices são menores que as tabelas e, por essa razão, ocupam relativamente pouco espaço na memória principal.
 
